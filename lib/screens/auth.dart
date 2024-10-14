58f18f444657c23baa204688a9990360eb8b9bab
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-10-14 11:07:12 +0200

    add validation to authentication form

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -12,16 +12,83 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   //chiave globale per il Form
   final _formKey = GlobalKey<FormState>();
-
   //controller per gli input
   final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _cognomeController = TextEditingController();
+  final TextEditingController _nickNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  bool _obscurePassword = true;
 
-//toggle per la visivilita della password
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
+
+//VALIDAZIONE CAMPI INPUT
+
+  //validazione nome congome e Nickname
+  String? _validateField(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Campo obbligatorio';
+    }
+    return null;
+  }
+
+  //validazione mail
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Inserisci una Email';
+    }
+    const emailRegex = r'^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$';
+    if (!RegExp(emailRegex).hasMatch(value)) {
+      return 'Inserisci un\'email valida';
+    }
+    return null;
+  }
+
+  //validazione password
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Inserisci una password';
+    } else if (value.length < 8) {
+      return 'La password deve avere almeno 8 caratteri';
+    } else if (!RegExp(r'[a-z]').hasMatch(value)) {
+      return 'La password deve contenere almeno una lettera minuscola';
+    } else if (!RegExp(r'[A-Z]').hasMatch(value)) {
+      return 'La password deve contenere almeno una lettera maiuscola';
+    } else if (!RegExp(r'[0-9]').hasMatch(value)) {
+      return 'La password deve contenere almeno un numero';
+    } else if (!RegExp(r'[!@#$%&*?]').hasMatch(value)) {
+      return 'La password deve contenere almeno un carattere speciale';
+    }
+    return null;
+  }
+
+  //validazione password
+  String? _validateConfirmPassword(String? value) {
+    if (value != _passwordController.text) {
+      return 'Le password non coincidono';
+    }
+    return null;
+  }
+
+//FUNZIONE PER INVIARE IL FORM
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      print('form completto pronto per Invio');
+    }
+  }
+
+//TOOGLE per la visivilita della password
   void _toggleObscurePassword() {
     setState(() {
       _obscurePassword = !_obscurePassword;
+    });
+  }
+
+  void _toggleConfirmObscurePassword() {
+    setState(() {
+      _obscureConfirmPassword = !_obscureConfirmPassword;
     });
   }
 
@@ -32,23 +99,90 @@ class _AuthScreenState extends State<AuthScreen> {
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text('LogIn-SingUp'),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(18),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Authform.buildTextField(
-                    labelText: 'Nome', controller: _nomeController),
-                const SizedBox(height: 10),
-                Authform.buildTextField(
-                  labelText: 'Password',
-                  controller: _passwordController,
-                  obscureText: _obscurePassword,
-                  toggleobscureText: _toggleObscurePassword,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme.of(context).primaryColor,
+              const Color.fromARGB(193, 30, 87, 185),
+            ],
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            //padding: EdgeInsets.all(18),
+            child: Form(
+              key: _formKey,
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color:
+                          const Color.fromARGB(255, 9, 9, 9).withOpacity(0.3),
+                      spreadRadius: -17,
+                      blurRadius: 15,
+                      offset: Offset(0, 0),
+                    ),
+                  ],
                 ),
-              ],
+                child: Card(
+                  margin: const EdgeInsets.all(20),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 36.0),
+                    child: Column(
+                      children: [
+                        Authform.buildTextField(
+                          labelText: 'Nome',
+                          controller: _nomeController,
+                          validator: _validateField,
+                        ),
+                        Authform.buildTextField(
+                          labelText: 'Cognome',
+                          controller: _cognomeController,
+                          validator: _validateField,
+                        ),
+                        Authform.buildTextField(
+                          labelText: 'NickName',
+                          controller: _nickNameController,
+                          validator: _validateField,
+                        ),
+                        Authform.buildTextField(
+                          labelText: 'Email',
+                          controller: _emailController,
+                          validator: _validateEmail,
+                        ),
+                        Authform.buildTextField(
+                            labelText: 'Password',
+                            controller: _passwordController,
+                            isPasswordField: true,
+                            obscureText: _obscurePassword,
+                            toggleobscureText: _toggleObscurePassword,
+                            validator: _validatePassword),
+                        Authform.buildTextField(
+                          labelText: 'Confirm Password',
+                          controller: _confirmPasswordController,
+                          isPasswordField: true,
+                          obscureText: _obscureConfirmPassword,
+                          toggleobscureText: _toggleConfirmObscurePassword,
+                          validator: _validateConfirmPassword,
+                        ),
+                        ElevatedButton(
+                          onPressed: _submitForm,
+                          child: Text('Registrati'),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text('Hai gia un account.'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -7,23 +7,31 @@ class Authform {
     String? Function(String?)? validator,
     bool obscureText = false,
     Function()? toggleobscureText,
+    bool isPasswordField = false,
   }) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-      child: TextFormField(
-        controller: controller,
-        decoration: InputDecoration(
-            labelText: labelText,
-            border: const OutlineInputBorder(),
-            suffixIcon: obscureText
-                ? IconButton(
-                    icon: Icon(
-                        obscureText ? Icons.visibility : Icons.visibility_off),
-                    onPressed: () {},
-                  )
-                : null),
-        validator: validator,
-        obscureText: obscureText,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: labelText,
+              border: const OutlineInputBorder(),
+              suffixIcon: isPasswordField
+                  ? IconButton(
+                      icon: Icon(obscureText
+                          ? Icons.visibility
+                          : Icons.visibility_off),
+                      onPressed: toggleobscureText,
+                    )
+                  : null,
+            ),
+            validator: validator,
+            obscureText: obscureText,
+          ),
+          const SizedBox(height: 10),
+        ],
       ),
     );
   }
